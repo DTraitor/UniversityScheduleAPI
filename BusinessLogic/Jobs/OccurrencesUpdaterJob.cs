16a671c29671b95aca00f366ff2d70f9cd2534be
DTraitor <sehhd32@gmail.com>
2025-09-25 22:55:05 +0300

    saturdays

--- BusinessLogic/Jobs/OccurrencesUpdaterJob.cs
+++ BusinessLogic/Jobs/OccurrencesUpdaterJob.cs
@@ -11,6 +11,10 @@ namespace BusinessLogic.Jobs;
 
 public class OccurrencesUpdaterJob : IHostedService, IDisposable, IAsyncDisposable
 {
+    private readonly DateTime _saturdayMoveStart = DateTime.Parse("2025-12-01T00:00:00.000000+02:00");
+    private readonly DateTime _saturdayMoveEnd = DateTime.Parse("2025-12-14T23:59:59.999999+02:00");
+    private readonly DateTime _saturdayMoveToStart = DateTime.Parse("2025-09-06T00:00:00.000000+03:00");
+
     private readonly IServiceProvider _serviceProvider;
     private readonly ILogger<OccurrencesUpdaterJob> _logger;
     private Timer _timer;
@@ -46,7 +50,8 @@ public class OccurrencesUpdaterJob : IHostedService, IDisposable, IAsyncDisposab
             var userLessonRepository = scope.ServiceProvider.GetRequiredService<IUserLessonRepository>();
             var userLessonOccurenceRepository = scope.ServiceProvider.GetRequiredService<IUserLessonOccurenceRepository>();
 
-            var lessonsToUpdate = userLessonRepository.GetWithOccurrencesCalculatedDateLessThan(DateTimeOffset.UtcNow.AddDays(90));
+            var lessonsToUpdate = userLessonRepository.GetWithOccurrencesCalculatedDateLessThan(DateTimeOffset.UtcNow.AddDays(90))
+                .Where(x => x.OccurrencesCalculatedTill == null || x.OccurrencesCalculatedTill < x.EndTime);
 
             List<UserLessonOccurrence> userLessonOccurrences = new List<UserLessonOccurrence>();
 
@@ -56,22 +61,26 @@ public class OccurrencesUpdaterJob : IHostedService, IDisposable, IAsyncDisposab
             {
                 TimeZoneInfo timeZone = TimeZoneInfo.FindSystemTimeZoneById(lesson.TimeZoneId);
 
-                var occurrence = userLessonOccurenceRepository.GetLatestOccurrence(lesson.Id);
                 DateTime? latestOccurrence;
-                if (occurrence == null)
+                if (lesson.OccurrencesCalculatedTill == null)
                     latestOccurrence = lesson.RepeatType == RepeatType.Never ? null : TimeZoneInfo.ConvertTime(lesson.StartTime, timeZone).DateTime;
                 else
-                    latestOccurrence = lesson.RepeatType.GetNextOccurrence(TimeZoneInfo.ConvertTime(occurrence.StartTime, timeZone).DateTime, lesson.RepeatCount);
+                    latestOccurrence = lesson.RepeatType.GetNextOccurrence(TimeZoneInfo.ConvertTime(lesson.OccurrencesCalculatedTill.Value, timeZone).DateTime, lesson.RepeatCount);
 
                 while (latestOccurrence != null && latestOccurrence < lesson.EndTime && latestOccurrence < limit)
                 {
-                    userLessonOccurrences.Add(new UserLessonOccurrence
+                    if (!HandleSaturdays(latestOccurrence.Value, lesson, out var occurence))
                     {
-                        LessonId = lesson.Id,
-                        UserId = lesson.UserId,
-                        StartTime = TimeZoneInfo.ConvertTimeToUtc(latestOccurrence.Value.Add(lesson.BeginTime)),
-                        EndTime = TimeZoneInfo.ConvertTimeToUtc(latestOccurrence.Value.Add(lesson.BeginTime).Add(lesson.Duration)),
-                    });
+                        occurence = new UserLessonOccurrence
+                        {
+                            LessonId = lesson.Id,
+                            UserId = lesson.UserId,
+                            StartTime = TimeZoneInfo.ConvertTimeToUtc(latestOccurrence.Value.Add(lesson.BeginTime)),
+                            EndTime = TimeZoneInfo.ConvertTimeToUtc(latestOccurrence.Value.Add(lesson.BeginTime).Add(lesson.Duration)),
+                        };
+                    }
+
+                    userLessonOccurrences.Add(occurence);
 
                     latestOccurrence = lesson.RepeatType.GetNextOccurrence(latestOccurrence.Value, lesson.RepeatCount);
                 }
@@ -117,6 +126,27 @@ public class OccurrencesUpdaterJob : IHostedService, IDisposable, IAsyncDisposab
                 }
             }
         }
+    }
+
+    public bool HandleSaturdays(DateTime occurenceDate, UserLesson lesson, out UserLessonOccurrence? userLessonOccurrence)
+    {
+        userLessonOccurrence = null;
+        if (occurenceDate < _saturdayMoveStart || occurenceDate > _saturdayMoveEnd)
+            return false;
+
+        var diff = occurenceDate - _saturdayMoveStart;
+
+        var movedDate = _saturdayMoveToStart.AddDays(7 * (diff.Days));
+
+        userLessonOccurrence = new UserLessonOccurrence
+        {
+            LessonId = lesson.Id,
+            UserId = lesson.UserId,
+            StartTime = TimeZoneInfo.ConvertTimeToUtc(movedDate.Add(lesson.BeginTime)),
+            EndTime = TimeZoneInfo.ConvertTimeToUtc(movedDate.Add(lesson.BeginTime).Add(lesson.Duration)),
+        };
+
+        return true;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
