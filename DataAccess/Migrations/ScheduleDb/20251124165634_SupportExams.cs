acca7b72de18b8074494fdc123f1ea0050e4bb78
DTraitor <sehhd32@gmail.com>
2025-11-24 20:19:22 +0200

    migrations

---
+++ DataAccess/Migrations/ScheduleDb/20251124165634_SupportExams.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations.ScheduleDb
+{
+    /// <inheritdoc />
+    public partial class SupportExams : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "OneTimeOccurence",
+                table: "LessonEntries",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OneTimeOccurence",
+                table: "LessonEntries");
+        }
+    }
+}
